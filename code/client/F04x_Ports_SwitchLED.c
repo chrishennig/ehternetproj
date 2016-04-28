2fa8d6f08abe8ebc3565905b82682dc6b9cd931a
Timm K <TimmKaptein@gmail.com>
2016-04-28 15:29:25 +0200

    Programm gekürzt

--- code/client/F04x_Ports_SwitchLED.c
+++ code/client/F04x_Ports_SwitchLED.c
@@ -1,41 +1,7 @@
 //-----------------------------------------------------------------------------
 // F04x_Ports_SwitchLED.c
 //-----------------------------------------------------------------------------
-// Copyright 2005 Silicon Laboratories, Inc.
-// http://www.silabs.com
-//
-// Program Description:
-//
-// This program demonstrates how to configure port pins as digital inputs
-// and outputs.  The C8051F040 target board has one push-button switch 
-// connected to a port pin and one LED.  The program constantly checks the 
-// status of the switch and if it is pushed, it turns on the LED.
-//
-// The program also monitors P4.0.  If P4.0 is high, it sets P4.1 low.  If
-// P4.0 is low, P4.1 is set high.  The purpose of this part of the program
-// is to show how to access the higher ports.  Ports 4-7 are not available
-// on all SFR pages, so the SFRPAGE register must be set correctly before 
-// reading or writing these ports.
-//
-//
-// How To Test:
-//
-// 1) Download code to a 'F040 target board
-// 2) Ensure that the J1 and J3 headers are shorted
-// 3) Push the button (P3.7) and see that the LED turns on 
-// 4) Set P4.0 high and low using an external connection and check that
-//    the output on P4.1 is the inverse of P4.0.
-//
-//
-// FID:            04X000002
-// Target:         C8051F04x
-// Tool chain:     Keil C51 7.50 / Keil EVAL C51
-// Command Line:   None
-//
-// Release 1.0
-//    -Initial Revision (GP)
-//    -15 NOV 2005
-//
+
 
 //-----------------------------------------------------------------------------
 // Includes
@@ -162,27 +128,7 @@ void OSCILLATOR_Init (void)
 //-----------------------------------------------------------------------------
 // PORT_Init
 //-----------------------------------------------------------------------------
-//
-// Return Value : None
-// Parameters   : None
-//
-// This function configures the crossbar and ports pins.
-// 
-// To configure a pin as a digital input, the pin is configured as digital
-// and open-drain and the port latch should be set to a '1'.  The weak-pullups
-// are used to pull the pins high.  Pressing the switch pulls the pins low.
-//
-// To configure a pin as a digital output, the pin is configured as digital
-// and push-pull.  
-//
-// Some ports pins do not have the option to be configured as analog or digital,
-// so it not necessary to explicitly configure them as digital.
-//
-// An output pin can also be configured to be an open-drain output if system
-// requires it.  For example, if the pin is an output on a multi-device bus,
-// it will probably be configured as an open-drain output instead of a 
-// push-pull output.  For the purposes of this example, the pin is configured
-// as push-pull output because the pin in only connected to an LED.
+
 //
 // P1.6   digital   push-pull     LED1
 // P3.7   digital   open-drain    Switch 1
