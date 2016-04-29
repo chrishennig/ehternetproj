ca00e4b21c130479cb8df7f2d411cbd6c48ae533
Chris Hennig <hennigchris90@gmail.com>
2016-04-29 15:52:32 +0200

    [CLIENT] Änderungen

--- code/client/F04x_Ports_SwitchLED.c
+++ code/client/F04x_Ports_SwitchLED.c
@@ -1,39 +1,31 @@
-//-----------------------------------------------------------------------------
-// F04x_Ports_SwitchLED.c
-//-----------------------------------------------------------------------------
-
-
-//-----------------------------------------------------------------------------
 // Includes
-//-----------------------------------------------------------------------------
 
-#include <c8051f040.h>                 // SFR declarations
+#include <c8051f040.h>                       // SFR declarations
 
-//-----------------------------------------------------------------------------
-// Pin Declarations
-//-----------------------------------------------------------------------------
-
-
-//-----------------------------------------------------------------------------
 // Function Prototypes
-//-----------------------------------------------------------------------------
 
 void OSCILLATOR_Init (void);           
 void PORT_Init (void);
 void Readtimer (void);
-void updateNumbers (void);
 void Timer3_Init (int counts);
 void Timer3_ISR (void);// interrupt 14
 
-//############## Ethernet Frame ############## 
+//Variablen
 
+#define SYSCLK 3062500                       // approximate SYSCLK frequency in Hz
 
-sfr16 RCAP3    = 0xCA;                 // Timer3 reload value
-sfr16 TMR3     = 0xCC;                 // Timer3 counter
+sfr16 RCAP3    = 0xCA;                       // Timer3 reload value
+sfr16 TMR3     = 0xCC;                       // Timer3 counter
 sbit   LED     = P1^6;
-#define SYSCLK 3062500                   // approximate SYSCLK frequency in Hz
 
+char Tab7Seg[10]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+int  reset=0;
+int startrutine=0;
+int toggle=1;
+int framecounter = 1; 
 
+//############## Ethernet Frame ############## 
+char frame[32];
 char praeambel[7] = {0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
 char begin = 0xAB;
 char ziel[6] = {0x01, 0x01, 0x01, 0x01, 0x01, 0x01};
@@ -43,34 +35,17 @@ char daten[5];
 char pad = 0x00;
 char crc[4] = {0x00, 0x00, 0x00, 0x00};
 
-char frame[32];
-
-
-char Tab7Seg[10]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
-
-int  reset=0;
-int startrutine=0;
-int  x=0;
-int toggle=1;
-double counter;
-int framecounter = 1;  
-
-
-//-----------------------------------------------------------------------------
-// main() Routine
-//-----------------------------------------------------------------------------
-
 void main (void)
 {
 
-   WDTCN = 0xde;                       // Disable watchdog timer
+   WDTCN = 0xde;                             // Disable watchdog timer
    WDTCN = 0xad;
 
    SFRPAGE =CONFIG_PAGE;
-   PORT_Init();                        // Initialize Port I/O
-   OSCILLATOR_Init ();                 // Initialize Oscillator
+   PORT_Init();                              // Initialize Port I/O
+   OSCILLATOR_Init ();                       // Initialize Oscillator
    SFRPAGE =TMR3_PAGE;
-   Timer3_Init (SYSCLK/12/1);     //###########################################################################
+   Timer3_Init (SYSCLK/12/1);     
    EA=1;
    SFRPAGE= LEGACY_PAGE;
 
@@ -80,94 +55,54 @@ void main (void)
    {
    P1=P3;
    P2= frame[20];
-   }                                   // end of while(1)
-}                                      // end of main()
+   }                                         // end of while(1)
+}                                            // end of main()
 
-//-----------------------------------------------------------------------------
-// Initialization Subroutines
-//-----------------------------------------------------------------------------
-
-void updateNumbers(void)
-{
-   //P1 = framebuffer[24];
-}
-
-
-//-----------------------------------------------------------------------------
-// OSCILLATOR_Init
-//-----------------------------------------------------------------------------
-//
-// Return Value : None
-// Parameters   : None
-//
-// This function initializes the system clock to use the internal oscillator
-// at its maximum frequency.
-//
-//-----------------------------------------------------------------------------
 void OSCILLATOR_Init (void)
 {
-   char SFRPAGE_SAVE = SFRPAGE;        // Save Current SFR page
+   char SFRPAGE_SAVE = SFRPAGE;              // Save Current SFR page
 
-   SFRPAGE = CONFIG_PAGE;              // set SFR page
+   SFRPAGE = CONFIG_PAGE;                    // set SFR page
 
-   OSCICN |= 0x03;                     // Configure internal oscillator for
-                                       // its maximum frequency (24.5 Mhz)
+   OSCICN |= 0x03;                           // Configure internal oscillator for
+                                             // its maximum frequency (24.5 Mhz)
 
-   SFRPAGE = SFRPAGE_SAVE;             // Restore SFR page
+   SFRPAGE = SFRPAGE_SAVE;                   // Restore SFR page
 }
-//-----------------------------------------------------------------------------
-// PORT_Init
-//-----------------------------------------------------------------------------
 
-//
-// P1.6   digital   push-pull     LED1
-// P3.7   digital   open-drain    Switch 1
-// P4.0   digital   open-drain    Input 1
-// P4.1   digital   push-pull     Output 1
-//-----------------------------------------------------------------------------
 void PORT_Init (void)
 {
-   char SFRPAGE_SAVE = SFRPAGE;        // Save Current SFR page
+   char SFRPAGE_SAVE = SFRPAGE;              // Save Current SFR page
 
-   SFRPAGE = CONFIG_PAGE;              // set SFR page before writing to
-                                       // registers on this page
+   SFRPAGE = CONFIG_PAGE;                    // set SFR page before writing to
+                                             // registers on this page
 
-   //P1MDIN |= 0xff;                     // P1 is digital  muss 1
-   //P3MDIN |= 0xff;  
+   P1MDOUT = 0xff;                           // P1 is push-pull
+   P2MDOUT = 0xff;                           // P2 is push-pull
+   P3MDOUT = 0x00;                           // P3 is open-drain  jetzt P2
 
-   P1MDOUT = 0xff;                     // P1 is push-pull
-   P2MDOUT = 0xff;
-   P3MDOUT = 0x00;                     // P3 is open-drain  jetzt P2
+   XBR2    = 0x40;                           // Enable crossbar and enable
+                                             // weak pull-ups
 
-   //P3     |= 0x00;                     // Set P3 latch to '1'   soll raus
-
-   //P4MDOUT = 0x02;                     // P4.0 is open-drain; P4.1 is push-pull
-   //P4      = 0x01;                     // Set P4.1 latch to '1'
-
-
-   XBR2    = 0x40;                     // Enable crossbar and enable
-                                       // weak pull-ups
-
-   SFRPAGE = SFRPAGE_SAVE;             // Restore SFR page
+   SFRPAGE = SFRPAGE_SAVE;                   // Restore SFR page
 }
 
 void Timer3_Init (int counts)
 {
-   TMR3CN = 0x00;                      // Stop Timer3; Clear TF3;
-                                       // use SYSCLK/12 as timebase
-   RCAP3   = -counts;                  // Init reload values
-   TMR3    = 0xffff;                   // set to reload immediately
-   EIE2   |= 0x01;                     // enable Timer3 interrupts
-   TR3 = 1;                            // start Timer3
+   TMR3CN = 0x00;                            // Stop Timer3; Clear TF3;
+                                             // use SYSCLK/12 as timebase
+   RCAP3   = -counts;                        // Init reload values
+   TMR3    = 0xffff;                         // set to reload immediately
+   EIE2   |= 0x01;                           // enable Timer3 interrupts
+   TR3 = 1;                                  // start Timer3
 }
 
 
 void Timer3_ISR (void) interrupt 14
 {
    TF3 = 0;
-
 	 
-	if (toggle==1)
+	if (toggle==1)                             // hier wird der toggle zwischen 0 und einem wert behandelt
 	{
 	   if ( P3 == 0x00)
 	   {
@@ -177,26 +112,24 @@ void Timer3_ISR (void) interrupt 14
 	}
    if ( reset == 1)
    {
-		if (P3 != 0x00)            // Suche nach Preambel
+		if (P3 != 0x00)                         // Suche nach Preambel
 	 	{     
 			if (framecounter == 31)
 			{
 				framecounter=1;
-				//P2 = ~P2;
 			}
             toggle=1;
-			frame[framecounter] = P3;
+			frame[framecounter] = P3;            // hier wird in das Framefeld geschriben
 
 
 
 			if ( P3 == 0x55)
 				{ 
 					startrutine++;
-					if (startrutine == 7)  //  Abfrage ob Werte sinnvoll sind
+					if (startrutine == 7)          //  Abfrage ob 7 mal paeambel
 					{
 						framecounter = 7;
 						startrutine = 0;
-						//P2 = ~P2;
 					}
 				}
 			framecounter++;
@@ -204,7 +137,3 @@ void Timer3_ISR (void) interrupt 14
       	}
    }
 }
-
-//-----------------------------------------------------------------------------
-// End Of File
-//-----------------------------------------------------------------------------
